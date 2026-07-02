ec7b080226b9b95387d75770051c096d0d722356
furdog <138627600+furdog@users.noreply.github.com>
2026-07-02 19:42:30 +0300

    update: standard

--- template.h
+++ template.h
@@ -10,12 +10,16 @@
  * C89, Linux kernel style, MISRA, rule of 10, No hardware specific code,
  * only generic C and some binding layer. Be extra specific about types.
  *
- * Scientific units where posible at end of the names, for example:
- * - timer_10s (timer_10s has a resolution of 10s per bit)
- * - power_150w (power 150W per bit or 0.15kw per bit)
- *
- * Keep variables without units if they're unknown or not specified or hard
- * to define with short notation.
+ * Naming Convention Standard:
+ * Use scientific units with short qualifier prefixes where possible:
+ * Pattern: [property_description]_[qualifier][scale][unit]_[offset]_[type]
+ * Examples:
+ * - timer_r10ms_u8        (Resolution: 10ms/bit, Type: uint8_t)
+ * - power_r150w           (Resolution: 150W/bit or 0.15kwt/bit)
+ * - soc_r0p1pct_u16       (Resolution: 0.1%/bit, Type: uint16_t)
+ * - carbon_q5mol_u32      (Quantity:   5mol/unit, Type: uint32_t)
+ * - speed_r0p5kmh_offn500 (Resolution: 0.5km/h, Offset: -500)
+ * - prescaler_r1div7      (Resolution: 1/7 or 0,142857...)
  *
  * ```LICENSE
  * Copyright (c) 2025 furdog <https://github.com/furdog>
